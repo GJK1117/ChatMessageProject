553abbde8100b9896aac874102f75e27bf615d1b
GJK1117 <118426175+GJK1117@users.noreply.github.com>
2024-02-02 16:39:54 +0900

    feat: set basic project files
    set Spring Boot Project

---
+++ src/main/java/TestWebSocket/Jungon/JungonApplication.java
@@ -0,0 +1,13 @@
+package TestWebSocket.Jungon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JungonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TestWebSocket/Jungon/JungonApplicationTests.java
@@ -0,0 +1,13 @@
+package TestWebSocket.Jungon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
